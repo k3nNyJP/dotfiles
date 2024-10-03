790a451b246ecf8c55efd352ef37cd9dd2906221
k3nNyJP <k3nny.51rcy@gmail.com>
2024-10-03 10:21:50 +0900

    remove step to setup homebrew from initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# instal homebrew
-if ! type brew &> /dev/null ; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "Since Homebrew is already installed, skip this phase and proceed."
-fi
-brew bundle install --file=Brewfile
-
 # copy shell config
 ln -sf $(pwd)/.zshrc ~/.zshrc
 
