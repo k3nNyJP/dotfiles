02fb688956e92e511fc9990f75269ee2f5c733ab
k3nNyJP <k3nny.51rcy@gmail.com>
2024-10-04 20:56:58 +0900

    copy .npmrc physically

--- initialize.sh
+++ initialize.sh
@@ -7,5 +7,5 @@ ln -sf $(pwd)/.zshrc ~/.zshrc
 # copy git config
 ln -sf $(pwd)/.user_gitconfig ~/.gitconfig
 
-# copy npm config
-ln -sf $(pwd)/.npmrc ~/.npmrc
+# copy npm config (This file includes credential which needs to be written by hand)
+cp $(pwd)/.npmrc ~/.npmrc
